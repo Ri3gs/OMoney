f39e85253bf39d67adedeb156c5a015c978a936d
arthur.tyan <arthur.tyan@diatomenterprises.com>
2015-01-30 09:52:57 +0200

    OM-34 - Add login page.

---
+++ OMoney.Web/app/controllers/loginController.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    "use strict";
+    angular.module("oMoney").controller("loginController", ["$state", "userResource", loginController]);
+
+    function loginController($state, userResource) {
+        var vm = this;
+        vm.User = {};
+
+        vm.login = function () {
+            var user = new userResource(vm.User);
+            user.$save(function (data) {
+                $state.go("profile");
+            });
+        }
+
+        vm.cancel = function () {
+            $state.go("home");
+        }
+    };
+}());
---
+++ OMoney.Web/app/controllers/profileController.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    "use strict";
+    angular.module("oMoney").controller("profileController", ["$state", "userResource", profileController]);
+
+    function profileController($state, userResource) {
+        var vm = this;
+        vm.User = {};
+
+        vm.login = function () {
+            var user = new userResource(vm.User);
+            user.$save(function (data) {
+                $state.go("profile");
+            });
+        }
+
+        vm.cancel = function () {
+            $state.go("home");
+        }
+    };
+}());
