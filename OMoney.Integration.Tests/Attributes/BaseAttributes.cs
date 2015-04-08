69fde7bf7e343dacbb6b0542b57866a01fe767ec
nikolay.bilonenko <nikolay.bilonenko@diatomenterprises.com>
2015-04-08 21:33:55 +0300

    1.Add to integration tests attributes for golduser.
    2.Add to integration tests PhantomJS

--- OMoney.Integration.Tests/Attributes/BaseAttributes.cs
+++ OMoney.Integration.Tests/Attributes/BaseAttributes.cs
@@ -1,25 +1,34 @@
 ﻿using System;
 using NUnit.Framework;
+using OMoney.Integration.Tests.Extensions;
+using OpenQA.Selenium;
 
 namespace OMoney.Integration.Tests.Attributes
 {
-    class BaseAttributes : Attribute, ITestAction
+    abstract class BaseAttributes : Attribute, ITestAction
     {
         public void AfterTest(TestDetails testDetails)
         {
             BaseIntegrationTest test = testDetails.Fixture as BaseIntegrationTest;
-
+            LogOut(BaseIntegrationTest.Driver);
         }
 
         public void BeforeTest(TestDetails testDetails)
         {
             BaseIntegrationTest test = testDetails.Fixture as BaseIntegrationTest;
+            Login(BaseIntegrationTest.Driver);
 
         }
 
         public ActionTargets Targets
         {
             get { return ActionTargets.Test; }
+        }
+        public abstract void Login(IWebDriver driver);
+
+        public virtual void LogOut(IWebDriver driver)
+        {
+            driver.WaitForElement(By.XPath("/html/body/div/div[1]/div/nav/ul/li[4]/a")).ForceClick();
         }
     }
 }
--- OMoney.Integration.Tests/BaseIntegrationTest.cs
+++ OMoney.Integration.Tests/BaseIntegrationTest.cs
@@ -1,30 +1,26 @@
 ﻿using System;
 using System.Drawing;
 using NUnit.Framework;
-using OMoney.Data.Context;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.PhantomJS;
 using OpenQA.Selenium.Support.UI;
 
 namespace OMoney.Integration.Tests
 {
     public class BaseIntegrationTest
     {
-        public static IWebDriver Driver = new ChromeDriver();
-        //public AuthContext _authContext;
-        public WebDriverWait _wait;
+        //public static IWebDriver Driver = new ChromeDriver();
+        public static IWebDriver Driver = new PhantomJSDriver();
+        public WebDriverWait Wait;
 
 
         [TestFixtureSetUp]
         public void SetupTest()
         {
-            //_authContext = new AuthContext();
-            //var assignments = _authContext.AspNetUsers;
-            //_authContext.AspNetUsers.RemoveRange(assignments);
-            //_authContext.SaveChanges();
-            Driver.Manage().Window.Size = new Size(1280, 720);
-            _wait = new WebDriverWait(Driver, TimeSpan.FromHours(5));
-            // Driver.Manage().Window.Maximize();
+           Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(20));
+           Driver.Manage().Window.Size = new Size(1280, 720);
+           // Driver.Manage().Timeouts().ImplicitlyWait(new TimeSpan(0, 0, 1));
+           // Driver.Manage().Window.Maximize();
 
 
         }
@@ -32,8 +28,7 @@ namespace OMoney.Integration.Tests
         public void TeardownTest()
         {
             // Driver.Close();         
-            //Driver.Quit();
-            //_authContext.Dispose();
+            Driver.Quit();
         }
 
     }
