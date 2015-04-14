05f2ecf88a37ff705daa64335c3e86d0f49822d8
arthur.tyan <arthur.tyan@diatomenterprises.com>
2015-04-14 09:33:49 +0300

    Add Integration tests for empty login form.

--- OMoney.Integration.Tests/WhenWorkingWithLoginPage/AndLoginDataIsCorrect.cs
+++ OMoney.Integration.Tests/WhenWorkingWithLoginPage/AndLoginDataIsCorrect.cs
@@ -1,15 +1,10 @@
 ﻿using NUnit.Framework;
-using OMoney.Integration.Tests.Extensions;
 
 namespace OMoney.Integration.Tests.WhenWorkingWithLoginPage
 {
     [TestFixture]
     public class AndLoginDataIsCorrect : BaseIntegrationTest
     {
-        [Test]
-        public void AndLoginButtonIsDisabled()
-        {
-            Driver.NavigateToLoginPage();
-        }
+        
     }
 }
