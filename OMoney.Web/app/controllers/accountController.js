00395bf6d1bce89c9f9ca2d5e8c2faf7ee1b96d7
CAT_Caterpiller <caterpiller@ex.ua>
2015-04-15 14:53:34 +0300

    delete accounts with popups

--- OMoney.Web/app/controllers/accountController.js
+++ OMoney.Web/app/controllers/accountController.js
@@ -1,15 +1,20 @@
 ﻿(function () {
     'use strict';
-    angular.module('oMoney').controller('accountController', ['$scope', '$location', '$modal', 'accounts', 'accountsService', 'notificationService', function($scope, $location, $modal, accounts, accountsService, notificationService) {
+    angular.module('oMoney').controller('accountController', ['$scope', '$location', '$modal', '$route', 'accounts', 'accountsService', 'notificationService', function($scope, $location, $modal, $route, accounts, accountsService, notificationService) {
             $scope.accounts = accounts;
             $scope.deleteAccountViewModel = {};
 
             $scope.delete = function(id) {
-                $scope.deleteAccountViewModel.id = id;
-                accountsService.deleteAccount($scope.deleteAccountViewModel).then(function(response) {
-                    $location.path("/accounts");
-                }, function(response) {
-                    notificationService.exception(response.data);
+                
+                var modalInstance = $modal.open({
+                    templateUrl: 'app/templates/deleteAccountModal.html',
+                    controller: 'deleteAccountModalController',
+                    size: 'lg',
+                    resolve: {
+                         id: function () {
+                             return id;
+                         }
+                    }
                 });
             }
 
@@ -20,12 +25,6 @@
                     templateUrl: 'app/templates/createAccountModal.html',
                     controller: 'createAccountModalController',
                     size: 'lg'
-                });
-
-                modalInstance.result.then(function() {
-                    console.log("OK");
-                }, function() {
-                    console.log("CLOSED");
                 });
             }
 
