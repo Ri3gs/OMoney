e4d70790cc527b3b801312608df99e4e8c83e810
Oleksandr Dobrelia <sasha.nightwish@gmail.com>
2017-09-11 22:34:56 +0300

    marked old project files and .sln file as old

--- OMoney.Web/scripts/angular-translate-loader-static-files.js
+++ OMoney.Web/scripts/angular-translate-loader-static-files.js
@@ -67,9 +67,9 @@ function $translateStaticFilesLoader($q, $http) {
         ].join(''),
         method: 'GET',
         params: ''
-      }, options.$http)).success(function (data) {
+      }, options.$http)).then(function (data) {
         deferred.resolve(data);
-      }).error(function () {
+      }, function () {
         deferred.reject(options.key);
       });
 
